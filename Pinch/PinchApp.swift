fc3002386537911aa9a0dcb1da0ac4550125cbc8
Aayam Adhikari <thisisleo7@gmail.com>
2023-07-30 19:50:23 +0545

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Aayam Adhikari on 30/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
